e81d5869e987b1c989c250ced8813404ab41ffe2
bjeffrey92 <bjeffrey92@hotmail.com>
2024-06-01 17:35:37 +0100

    feat(model_training): module for training and xgboost model

---
+++ fpl_predictor/model_training/xgboost.py
@@ -0,0 +1,66 @@
+from functools import partial
+
+import polars as pl
+import xgboost as xgb
+from bayes_opt import BayesianOptimization
+from sklearn.metrics import mean_squared_error
+
+from fpl_predictor.model_training.load_data import load_data
+
+
+def train(X: pl.DataFrame, y: pl.Series, **kwargs) -> xgb.XGBRegressor:
+    if "n_estimators" in kwargs:
+        kwargs["n_estimators"] = int(kwargs["n_estimators"])
+    if "max_depth" in kwargs:
+        kwargs["max_depth"] = int(kwargs["max_depth"])
+    model = xgb.XGBRegressor(**kwargs, n_jobs=-1, random_state=1)
+    model.fit(X, y)
+    return model
+
+
+def train_and_evaluate(
+    train_X: pl.DataFrame,
+    train_y: pl.Series,
+    val_X: pl.DataFrame,
+    val_y: pl.Series,
+    **kwargs,
+) -> float:
+    model = train(train_X, train_y, **kwargs)
+    val_predictions = model.predict(val_X)
+    return -mean_squared_error(val_y, val_predictions)
+
+
+def optimise_hyperparameters(
+    train_X: pl.DataFrame,
+    train_y: pl.Series,
+    val_X: pl.DataFrame,
+    val_y: pl.Series,
+    init_points: int = 25,
+    n_iter: int = 50,
+) -> xgb.XGBRegressor:
+    pbounds = {
+        "n_estimators": (100, 1000),
+        "max_depth": (2, 25),
+        "min_child_weight": (1, 20),
+        "max_delta_step": (0, 25),
+        "learning_rate": (0.001, 0.5),
+        "gamma": (0, 10),
+        "reg_alpha": (0, 10),
+        "reg_lambda": (0, 20),
+    }
+    f = partial(
+        train_and_evaluate, train_X=train_X, train_y=train_y, val_X=val_X, val_y=val_y
+    )
+    optimizer = BayesianOptimization(
+        f=f,
+        pbounds=pbounds,
+        random_state=1,
+    )
+    optimizer.maximize(init_points=init_points, n_iter=n_iter)
+    best_model = train(train_X, train_y, **optimizer.max["params"])
+    return best_model
+
+
+def main():
+    data = load_data(n_prediction_weeks=2)
+    optimise_hyperparameters(data.train_X, data.train_y, data.val_X, data.val_y)
