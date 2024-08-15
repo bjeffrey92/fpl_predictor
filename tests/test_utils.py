07e6bb74f23ca681271f115616cf3dd3c4ba74a8
bjeffrey92 <bjeffrey92@hotmail.com>
2024-08-15 17:17:54 +0100

    test(squad_selection): tests for new module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,12 @@
-from fpl_predictor.utils import *  # noqa: F403
+from unittest import mock
+
+from fpl_predictor import utils
 
 
-def test_code_valid() -> None:
-    assert True
+def test_get() -> None:
+    url = "https://example.com"
+    with mock.patch.object(utils.requests, "get") as mock_get:
+        output = utils.get(url)
+        mock_get.assert_called_once_with(url)
+        mock_get.return_value.raise_for_status.assert_called_once()
+        assert output == mock_get.return_value.json.return_value
